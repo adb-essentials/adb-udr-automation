8b32233ee4a1affd4cdd49b1fb77f4ba77282db4
Bruce Nelson <bnelson.rugger@gmail.com>
2022-06-29 07:49:04 -0700

    updates to code
    updated python code to fix table 1 and 2 <BR/> splits and added a service tag validation

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import json
 import socket
+import ipaddress
 import subprocess
 
 import pandas as pd
@@ -24,18 +25,68 @@ def get_az_regions_dict() -> dict:
 
     return az_region_dict
 
+def get_srvc_tag_cidrs():
+    cidr_ranges = []
+    custom_command = "az network list-service-tags --location eastus --query \"values[?contains(name, 'Databricks')].[properties.addressPrefixes][][]\""
+    run_custom_command = subprocess.run(
+       custom_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-def get_ip_list_whitelist(domain_str: str) -> list:
+    # reading output and error
+    execute_stdout = run_custom_command.stdout.decode("utf-8")
+    cidr_ranges_raw = json.loads(execute_stdout)
+    for cidr in cidr_ranges_raw:
+        try:
+            ipaddress.IPv4Network(cidr)
+        except:
+            print("Tags : " + cidr + " is not a valid V4 ip")
+        else:
+            cidr_ranges.append(cidr)
+    return cidr_ranges
+
+def get_srvc_tag_count(cidrs: list, srvctags: list):
+    cidr_count = 0
+    cidr_count_raw = len(cidrs)
+    if cidr_count_raw == 0 : 
+        return "n/a"
+
+    is_srvctag = 'no'
+    for cidr in cidrs:
+        for srvctag_ip in srvctags:
+            n1 = ipaddress.IPv4Network(cidr)
+            n2 = ipaddress.IPv4Network(srvctag_ip)
+            if n1.overlaps(n2) :
+                cidr_count = cidr_count + 1 
+
+    #if cidr_count > 0 : 
+
+    if cidr_count == cidr_count_raw :
+        is_srvctag = 'yes'
+
+    return is_srvctag
+
+def get_ip_list_whitelist(domain_str: str, table_num: int) -> list:
     # get ip list behind the given domain str
     ip_list = []
-    try:
-        ais = socket.getaddrinfo(domain_str, 0, 0, 0, 0)
-        for result in ais:
-            ip_list.append(result[-1][0])
-        ip_list = list(set(ip_list))
-    except:
-        ip_list = []
+    ip_list_raw = []
 
+    for dom in domain_str.split(' and ') :
+        try:
+            ais = socket.getaddrinfo(dom, 0, 0, 0, 0)
+            for result in ais:
+                ip_list.append(result[-1][0] + '/32')
+            ip_list_raw = list(set(ip_list))
+        except:
+            ip_list_raw.append(dom)
+            
+    ip_list = []        
+    for cidr in ip_list_raw:
+        try:
+            ipaddress.IPv4Network(cidr)
+        except:
+            print("Lookup : " + cidr + " did not resolve a valid ip")
+        else:
+            ip_list.append(cidr)
+    
     return ip_list
 
 
